1155dbf1cfbdd644cb24e52cc697181764f15bb5
denysoncardoso <102764760+denysoncardoso@users.noreply.github.com>
2023-09-19 20:05:33 -0300

    iniciando meu projeto c#

--- dev_screen_sound/Program.cs
+++ dev_screen_sound/Program.cs
@@ -1,8 +1,26 @@
 ﻿string mensagemDeBoasVindas = "@\r\n░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░\r\n██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗\r\n╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║\r\n░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║\r\n██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝\r\n╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
 
-void ExibeMensagem()
+void ExibirMensagemdeBoasVindas()
 {
     Console.WriteLine(mensagemDeBoasVindas);
 }
 
-ExibeMensagem();
+void ExibirOpcoesdoMenu() 
+{ 
+    Console.WriteLine("\nDigite 1 para registrar uma banda"); 
+    Console.WriteLine("Digite 2 para mostrar todas as bandas"); 
+    Console.WriteLine("Digite 3 para avaliar uma banda"); 
+    Console.WriteLine("Digite 4 para exibir a media de uma banda"); 
+    Console.WriteLine("Digite 0 para sair");
+    Console.WriteLine("\nDigite sua opção: ");
+    
+    int opcaoEscolhida = Console.Read()!;
+    
+    if(opcaoEscolhida == 1)
+    {
+        Console.WriteLine("Voce digitou a opçao " + opcaoEscolhida);
+    }
+}
+
+ExibirMensagemdeBoasVindas();
+ExibirOpcoesdoMenu();
